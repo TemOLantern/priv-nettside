eafc19d54ae594b64b43f307e3ade2f84abd883d
Quiescent <bluevs10@hotmail.no>
2021-05-12 16:05:10 +0200

    readme

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,12 @@
 <script>
 </script>
 
-<div class="w-full h-full bg-opacity-10 bg-black">
-	<h1 class="justify-self-center text-align-center">Fabian Tangen</h1>
+<div class="w-full h-full bg-blue-100 bg-opacity-5">
+	<div class="absolute inset-0 flex items-center justify-center">
+	<span class="filter drop-shadow-lg text-6xl bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 text-center">
+	Fabian Tangen
+	</span>
+	</div>
 </div>
 
 <div class="w-full h-full bg-gray-500">
